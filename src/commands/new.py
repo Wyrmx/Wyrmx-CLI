96632814eb6a9d5bfeb09ea95fd738fcfa2cf05c
Yousri Aouledissa <aouledissayousri@gmail.com>
2025-07-06 20:44:18 +0100

    folder creation and virtual environment

--- src/commands/new.py
+++ src/commands/new.py
@@ -1,8 +1,36 @@
+import subprocess
+import sys
 import typer
-
+from pathlib import Path
 
 def new(project_name: str):
+
+
+    def createProjectFolder(projectName: str):
+        projectPath = Path(projectName)
+
+        try:
+            projectPath.mkdir(parents=True, exist_ok=False)
+            typer.echo(f"Created project folder: {projectPath.resolve()}")
+        except FileExistsError:
+            typer.echo(f"Error: Folder '{projectName}' already exists.")
+
+
+    def createVirtualEnvironment(projectName: str):
+        projectPath = Path(projectName)
+
+        subprocess.run(
+            [sys.executable, "-m", "venv", str(projectPath)],
+            check=True
+        )
+
+
     """
     Create a new Wyrmx project.
     """
-    typer.echo(f"Initializing Wyrmx project: {project_name}")
+    projectName: str = project_name
+
+
+    typer.echo(f"Initializing Wyrmx project: {projectName}")
+    createProjectFolder(projectName)
+    createVirtualEnvironment(projectName)
