0177419f54578fe9a7633982baa450b4b4c38081
Yousri Aouledissa <aouledissayousri@gmail.com>
2025-07-08 12:26:53 +0100

    fixed version command production bug

--- wyrmx/commands/version.py
+++ wyrmx/commands/version.py
@@ -1,10 +1,9 @@
 from pathlib import Path
-import tomllib
 import typer
 
 __version__ = "1.0.14"
 
-def version(value: bool):
+def version():
     typer.echo(f"Wyrmx CLI Version: {__version__}")
     raise typer.Exit()
 
